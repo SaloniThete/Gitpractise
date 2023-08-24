9a2155462fc61c7f35759452a4fc774a502ffe49
Saloni_Thete <Saloni662001@gmail.com>
2023-08-24 14:50:49 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class sum{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=3;
+        int c=a*b;
+        System.out.println("Multiplication is:"+c);
+    }
+}
